f05564d6b2bb83fb51be9b84ac63f2621a2e14a0
aldibergjr <agcj2@cin.ufpe.br>
2021-07-24 16:12:09 -0300

    Parsed program options

--- WeightedSampling.cpp
+++ WeightedSampling.cpp
@@ -2,7 +2,19 @@
 #include <vector>
 #include <sstream>
 #include <fstream>
+
 using namespace std;
+
+
+
+struct CONFIGS {
+    int id_col;
+    int weight_col;
+    int search_col;
+    string filter_param;
+    int size;
+    string filename;
+};
 
 vector<string> readCsvLine(istream& str)
 {
@@ -24,14 +36,40 @@ vector<string> readCsvLine(istream& str)
     return result;
 }
 
+CONFIGS parseArgs(int argc, char * argv[]){
+    CONFIGS appConfig;
+    for(int i = 1; i < argc; i++){
+        string paramName = argv[i];
+        if(paramName.compare("--id") == 0){
+            appConfig.id_col = stoi(argv[++i]);
+        }else if(paramName.compare("--weight") == 0){
+            appConfig.weight_col = stoi(argv[++i]);
+        }else if(paramName.compare("--size") == 0){
+            appConfig.size = stoi(argv[++i]);
+        }else if(paramName.compare("--filter") == 0){
+            appConfig.search_col = stoi(argv[++i]);
+            appConfig.filter_param = argv[++i];
+        }else {
+            appConfig.filename = argv[i];
+        }
+
+    }
+    return appConfig;
+}
+
 int main(int argc, char * argv[]) 
 {
     ifstream myFile("./network_flows.csv");
-    getNextLineAndSplitIntoTokens(myFile);
-    vector<string> mcsv = getNextLineAndSplitIntoTokens(myFile);
+    readCsvLine(myFile);
+    vector<string> mcsv = readCsvLine(myFile);
+    CONFIGS appConfig = parseArgs(argc, argv);
+    // for (std::vector<string>::const_iterator i = mcsv.begin(); i != mcsv.end(); ++i)
+    //     std::cout << *i << ' ';
+    cout << appConfig.filename << ' ' << appConfig.filter_param << ' ' << appConfig.id_col << ' ' << appConfig.search_col << ' ' << appConfig.size << ' ' << appConfig.weight_col << '\n';
     
-    for (std::vector<string>::const_iterator i = mcsv.begin(); i != mcsv.end(); ++i)
-        std::cout << *i << ' ';
     return 0;
 }
+
+
+
 
