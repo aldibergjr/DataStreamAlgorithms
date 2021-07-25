73321112a05ac48009e1d8ee57adbeb5586e73ef
aldibergjr <agcj2@cin.ufpe.br>
2021-07-25 12:53:49 -0300

    Read complete csv and params

--- WeightedSampling.cpp
+++ WeightedSampling.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
 #include <vector>
+#include <ctime>
+#include <cstdlib>
 #include <sstream>
 #include <fstream>
+#include<bits/stdc++.h> 
 
 using namespace std;
 
 
+
+
+struct net_flow{
+    string id;
+    int weight;
+    string param;
+} ;
+
+struct netComparator{
+    bool operator()(const net_flow& a,const net_flow& b) const{
+        return a.weight<b.weight;
+    }
+};
 
 struct CONFIGS {
     int id_col;
@@ -15,6 +31,8 @@ struct CONFIGS {
     int size;
     string filename;
 };
+
+CONFIGS appConfig;
 
 vector<string> readCsvLine(istream& str)
 {
@@ -35,6 +53,10 @@ vector<string> readCsvLine(istream& str)
     }
     return result;
 }
+
+
+
+
 
 CONFIGS parseArgs(int argc, char * argv[]){
     CONFIGS appConfig;
@@ -57,15 +79,136 @@ CONFIGS parseArgs(int argc, char * argv[]){
     return appConfig;
 }
 
+struct sketch{
+    vector<net_flow> H;
+    vector<net_flow> C;
+    float t;
+};
+
+sketch sample;
+
+sketch update (sketch sample, net_flow e){
+    int s = appConfig.size;
+    int sketch_size = sample.H.size() + sample.C.size();
+    vector<net_flow> h = sample.H;
+    vector<net_flow> c = sample.C;
+
+    if(sketch_size < s){
+        h.push_back(e);
+        
+        sample.H = h;
+        return sample;
+    }
+
+  
+
+    float wl = sample.t * c.size();
+    vector<net_flow> newL;
+    if(e.weight < sample.t){
+        newL.push_back(e);
+        wl += e.weight;
+    }else{
+        h.push_back(e);
+    }
+
+    make_heap(h.begin(), h.end(), netComparator());
+    sort_heap(h.begin(), h.end(),netComparator());
+
+    //extract funtion prob
+    cout<< h[0].weight <<endl;
+   
+    
+    while(h.size() != 0 && wl >= (s-h.size()) * h[0].weight){
+        net_flow min_e ;
+        min_e.id = h[0].id;
+        min_e.weight = h[0].weight;
+        h.erase(h.begin());
+        newL.push_back(min_e);
+        wl += min_e.weight;
+        
+    }
+
+    sample.t = (float)wl/(s-h.size());
+    /*
+    //extract func
+    float p_sum = 0;
+    for(int i = 0; i < c.size(); i++){
+        p_sum+= 1 - c[i].weight/sample.t;
+    }
+    for(int i = 0; i < newL.size(); i++){
+        p_sum+= 1 - newL[i].weight/sample.t;
+    }
+    float p = (float) rand()/RAND_MAX;
+    int j = 0;
+    if(c.size() > 0){
+         while(p >= 0 && j < newL.size()){
+            p -= 1 - newL[j].weight/sample.t;
+            j++;
+        }
+        if(p <= 0){
+            newL.erase(newL.begin() + j-1);
+        }else{
+            c.erase(newL.begin() + (rand() % c.size()));
+        }
+        
+    }
+
+   for(int i = 0; i < newL.size(); i++){
+            c.push_back(newL[i]);
+    }
+    */
+    
+    sample.H = h;
+    sample.C = c;
+    return sample;
+}
+
+vector<net_flow> readCSV( string input_f){
+    ifstream input(input_f);
+    //read headers 
+    
+    readCsvLine(input);
+
+
+
+    vector<net_flow> data;
+    while(input.peek() != EOF){
+        net_flow data_line;
+        vector<string> line = readCsvLine(input);
+        
+       
+        data_line.id = line[appConfig.id_col];
+        data_line.param = line[appConfig.search_col];
+        data_line.weight = stoi(line[appConfig.weight_col]);
+        sample = update(sample, data_line);
+        // if(sample.H.size() > appConfig.size)
+        //     cout << sample.t << endl;
+        // data.push_back(data_line);
+        
+            
+    }
+    return data;
+}
+
 int main(int argc, char * argv[]) 
 {
-    ifstream myFile("./network_flows.csv");
-    readCsvLine(myFile);
-    vector<string> mcsv = readCsvLine(myFile);
-    CONFIGS appConfig = parseArgs(argc, argv);
-    // for (std::vector<string>::const_iterator i = mcsv.begin(); i != mcsv.end(); ++i)
-    //     std::cout << *i << ' ';
-    cout << appConfig.filename << ' ' << appConfig.filter_param << ' ' << appConfig.id_col << ' ' << appConfig.search_col << ' ' << appConfig.size << ' ' << appConfig.weight_col << '\n';
+   srand(time(0));
+    
+    //read headers
+    
+
+    
+    appConfig = parseArgs(argc, argv);
+    vector<net_flow> data = readCSV(appConfig.filename);
+
+     
+    
+    
+
+     for (std::vector<net_flow>::const_iterator i = sample.H.begin(); i != sample.H.end(); ++i)
+            std::cout << ((net_flow)*i).id << ' ' << ((net_flow)*i).weight <<  '\n';
+
+
     
     return 0;
 }
