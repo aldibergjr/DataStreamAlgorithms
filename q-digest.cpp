ace6ae71091947e01060bdbe6f7cf7bf77d1f6e0
acrc2 <acrc2@cin.ufpe.br>
2021-08-22 21:36:33 -0300

    add result format and input choice

--- q-digest.cpp
+++ q-digest.cpp
@@ -29,7 +29,7 @@ struct CONFIGS {
     string query_args;
     string filename;
     string execution;
-    float query;
+    vector<double> queryValues;
     bool compare;
     bool help_exec;
 };
@@ -51,13 +51,15 @@ CONFIGS parseArgs(int argc, char * argv[]){
         //     appConfig.query_args = paramName;
         }else if(paramName.compare("rank") == 0 || paramName.compare("quant") == 0){
             appConfig.execution = paramName;
-            appConfig.query = atof(argv[++i]);
+            // appConfig.query = atof(argv[++i]);
         }else if(paramName.compare("-h") == 0 || paramName.compare("--help") == 0){
             appConfig.help_exec = true;
         // }else if(paramName.compare("--compare") == 0){
         //     appConfig.compare = true;
-        }else {
+        }else if(appConfig.filename==""){
             appConfig.filename = argv[i];
+        } else {
+            appConfig.queryValues.push_back(atof(argv[i]));
         }
 
     }
@@ -117,7 +119,6 @@ void Qdigest::update(long long x, long long w){
         long long avail = capacity - v->weight;
         long long added = min(w,avail);
         v->weight += added;
-        // cout<<capacity<<endl;
         w = w - added;
         long long mid = (l+r)/2;
         
@@ -159,7 +160,6 @@ long long Qdigest::rank(long long x){
 }
 
 pair<Node*,long long> _compress(Node* crawl,  long long capacity, long long avail_up){
-    // cout<<"oi"<<endl;
     cout<<crawl->weight<<endl;
 
     long long move_up = 0;
@@ -202,7 +202,6 @@ void Qdigest::compress(){
 
 long long Qdigest::quantile(double q, long long streamSize){
     long long target = round(streamSize * q);
-    cout<<target<<endl;
     long long l = 0, r = univ;
 
     long long x = (r+l)/2;
@@ -261,56 +260,38 @@ void ExecuteQD(){
     random_device rd;
     mt19937 gen(rd());
 
-    // long long univ = 256;
-    // int max_w = 50;
-    // double eps = 0.1;
-    // int n = 1000;
     vector<pair<long long,long long>> stream = readCSV(appConfig.filename);
-    
-    // for (int i=0;i<n;i++){
-    //     int x = uniform_int_distribution<long long>(1, univ)(gen);
-    //     int w = uniform_int_distribution<long long>(1, max_w)(gen);
-    //     stream.push_back({x,w});
-    // }
-    // stream.push_back({3,29});
-    // stream.push_back({2, 9});
-    // stream.push_back({2, 44});
-    // stream.push_back({6, 200});
-    // stream.push_back({1, 28});
-    // stream.push_back({1, 48});
     
     vector<long long>true_ranks(appConfig.universe_size+1,0);
     long long total_weight = 0;
 
     for(auto x:stream){
-        // cout<<x.first<<" "<<x.second<<endl;
         total_weight +=x.second;
         true_ranks[x.first] += x.second;
     }
-    // cout<<endl;
 
     for(int i=1;i<=appConfig.universe_size;i++){
-        // cout<<i<<" "<<true_ranks[i]<<endl;
         true_ranks[i] += true_ranks[i-1];
     }
-    // cout<<endl;
 
-    for(int x=1;x<=appConfig.universe_size;x++){
-        cout<<"x = "<<x<<endl;
-        cout<<"rank: "<<sketch.rank(x)<<" ";
-        cout<<"true rank: "<<true_ranks[x-1]<<endl;
-        cout<<"error: "<<true_ranks[x-1] - sketch.rank(x)<<" expected max error: "<<total_weight*appConfig.eps<<endl;
-        cout<<"------"<<endl;
+    if(appConfig.execution=="rank"){
+        for(auto x:appConfig.queryValues){
+            cout<<"Estimated Rank "<<x<<": "<<sketch.rank(x)<<" ";
+            cout<<"Real Rank"<<x<<": "<<true_ranks[x-1]<<endl;
+            cout<<"------"<<endl;
+        }
+    }else if(appConfig.execution=="quant"){
+        for(auto x:appConfig.queryValues){
+            cout<<"Estimated Rank for q: "<<x<<", ";
+            cout<<"targert Rank: "<<(long long)round(stream.size() * x)<<endl;
+            long long result = sketch.quantile(x,stream.size()); 
+            cout<<sketch.quantile(x,stream.size())<<" "<<sketch.rank(result)<<endl;
+            cout<<"------"<<endl;
+        }
     }
-    // cout<<sketch.quantile(0.5,total_weight)<<endl;
-
-    // cout<<"estimated: "<<sketch.rank(4)<<endl;
-    // cout<<"real rank: "<<true_ranks[3]<<endl;
     
-    // traverse(sketch.root);
-    // cout<<endl<<endl;
-    // sketch.compress();
-    // traverse(sketch.root);
+
+
     return; 
 }
 
